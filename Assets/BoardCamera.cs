171c65bd6780724b093c2d3b974a383f90647c57
Connor <connor@playsidestudios.com>
2021-07-19 20:27:34 +1000

    Made a gradient generator and added a background

--- Assets/BoardCamera.cs
+++ Assets/BoardCamera.cs
@@ -6,6 +6,7 @@ public class BoardCamera : MonoBehaviour
 {
     [SerializeField]
     public Camera boardCamera;
+    public GameObject backgroundQuad;
 
     public void SetupCamera(BoardManager _boardManager)
     {
@@ -17,5 +18,14 @@ public class BoardCamera : MonoBehaviour
 
         //set position
         boardCamera.transform.position = camPosition;
+
+        //move back quad to backface minus a bit
+        float relativeClipPos = boardCamera.farClipPlane - 10f;
+        backgroundQuad.transform.position = camPosition + boardCamera.transform.forward * relativeClipPos;
+
+        //scale it relative to cam transform
+        float h = Mathf.Tan(boardCamera.fieldOfView * Mathf.Deg2Rad * 0.5f) * relativeClipPos * 2.0f;
+        backgroundQuad.transform.localScale = new Vector3(h * boardCamera.aspect, h, 1f);
+
     }
 }
---
+++ Assets/GradientMaker.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GradientMaker : EditorWindow
+{
+    static Gradient gradient = new Gradient();
+    static int textureWidth = 64;
+    static int textureHeight = 64;
+    static TextureFormat textureFormat = TextureFormat.RGB24;
+
+    private void OnGUI()
+    {
+        gradient = EditorGUILayout.GradientField("Gradient", gradient);
+
+        textureWidth = Mathf.Clamp(EditorGUILayout.IntField("Texture Width", textureWidth), 0, 4196);
+        textureHeight = Mathf.Clamp(EditorGUILayout.IntField("Texture Height", textureHeight), 0, 4196);
+        textureFormat = (TextureFormat)EditorGUILayout.EnumPopup("Texture Format", textureFormat);
+        
+        if (GUILayout.Button("Bake Texture"))
+        {
+            Texture2D outputTexture = new Texture2D(textureWidth, textureHeight, textureFormat, true);
+            for (int y = 0; y < textureHeight; y++)
+            {
+                for (int x = 0; x < textureWidth; x++)
+                {
+                    outputTexture.SetPixel(x, y, gradient.Evaluate((float)y / (float)textureHeight));
+                }
+            }
+
+            outputTexture.Apply();
+            byte[] textureData = outputTexture.EncodeToPNG();
+            
+            string dirPath = Application.dataPath;
+            string curDate = System.DateTime.Now.TimeOfDay.Ticks.ToString();
+
+            string fileName = $"{dirPath}\\gradient_{curDate}.png";
+            
+            System.IO.File.WriteAllBytes(fileName, textureData);
+
+            AssetDatabase.Refresh();
+            Debug.Log("Baked Gradient Texture to " + $"{dirPath}\\gradient_{curDate}.png");
+
+        }
+    }
+
+    [MenuItem("Window/GradientMaker")]
+    public static void OpenGradientMaker()
+    {
+        EditorWindow.GetWindow<GradientMaker>();
+    }
+
+}

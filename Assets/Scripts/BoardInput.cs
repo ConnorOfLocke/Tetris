4fbc0085d45ce3cc14c34ffb201b89615693612e
Connor <connor@playsidestudios.com>
2021-07-21 17:24:11 +1000

    Setup touch input for IOS and android

--- Assets/Scripts/BoardInput.cs
+++ Assets/Scripts/BoardInput.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 
 public class BoardInput : MonoBehaviour
 {
-    Action<Direction> onInputMove;
-    Action<bool> onInputRotate;
-    Action onInputSlam;
+    private Action<Direction> onInputMove;
+    private Action<bool> onInputRotate;
+    private Action onInputSlam;
+
+    private const float horizontalSwipeScreenPercentage = 0.075f; //in Screen space (swipeRation * ScreenWidth)
+    private const float verticalSwipeScreenPercentage = 0.01f; //in Screen space (swipeRation * ScreenWidth)
+    private float horizSwipeThreshold => horizontalSwipeScreenPercentage * Screen.width;
+    private float vertSwipeThreshold => verticalSwipeScreenPercentage * Screen.height;
+
+    private float tapTimeThreshold = 0.3f; //in seconds
+    private float slamSwipeTimeAllowed = 0.2f; //In Seconds
+    private float downMovesPerSecond = 32f; //Per Second
+
+    private DateTime timePressedDown;
+    private Vector2 posisitionPressStart;
+    private DateTime lastTimeMovedDown;
+
+    private bool hasSwipedHorizontally = false;
+    private bool hasSlammed = false;
 
     public void Initialise(Action<Direction> _onInputMove, Action<bool> _onInputRotate, Action _onInputSlam)
     {
+        Debug.Log("[BoardInput] Disabling multitouch here");
+        Input.multiTouchEnabled = false;
+
         onInputMove = _onInputMove;
         onInputRotate = _onInputRotate;
         onInputSlam = _onInputSlam;
@@ -18,7 +37,7 @@ public class BoardInput : MonoBehaviour
 
     public void Update()
     {
-        //#if UNITY_EDITOR || UNITY_STANDALONE
+#if UNITY_EDITOR || UNITY_STANDALONE
         if (Input.GetKeyDown(KeyCode.LeftArrow))
             onInputMove(Direction.Left);
         if (Input.GetKeyDown(KeyCode.RightArrow))
@@ -33,9 +52,80 @@ public class BoardInput : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
             onInputSlam();
-//#else
+#endif
 
-//#endif
+#if UNITY_ANDROID || UNITY_IOS
+        if (Input.touches.Length > 0)
+        {
+            Debug.Log($"[BoardInput] Touch {Input.touches[0].phase} {Input.touches[0].position}");
+            Vector2 curPos = Input.touches[0].position;
+            Vector2 swipeDelta = curPos - posisitionPressStart;
+
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                timePressedDown = DateTime.Now;
+                lastTimeMovedDown = DateTime.Now;
+
+                posisitionPressStart = curPos;
+                hasSwipedHorizontally = false;
+                hasSlammed = false;
+            }
+            else if (Input.touches[0].phase == TouchPhase.Moved)
+            {
+                if (!hasSlammed)
+                {
+                    if (swipeDelta.x > horizSwipeThreshold)
+                    {
+                        onInputMove(Direction.Right);
+                        hasSwipedHorizontally = true;
+                        posisitionPressStart = curPos;
+                    }
+                    else if (swipeDelta.x < -horizSwipeThreshold)
+                    {
+                        onInputMove(Direction.Left);
+                        hasSwipedHorizontally = true;
+                        posisitionPressStart = curPos;
+                    }
+                    else if (swipeDelta.y < -vertSwipeThreshold)
+                    {
+                        if ((DateTime.Now - timePressedDown).TotalSeconds > slamSwipeTimeAllowed ||
+                            hasSwipedHorizontally ||
+                            swipeDelta.x > Mathf.Abs(horizSwipeThreshold) * 0.5f)
+                        {
+                            if ((DateTime.Now - lastTimeMovedDown).TotalSeconds > (1.0f / downMovesPerSecond))
+                            {
+                                onInputMove(Direction.Down);
+                                lastTimeMovedDown = DateTime.Now.AddSeconds((1.0f / downMovesPerSecond));
+                            }
+                        }
+                        else 
+                        {
+                            hasSlammed = true;
+                            onInputSlam();
+                            posisitionPressStart = curPos;
+                        }
+                    }
+                }
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended)
+            {
+                if ((DateTime.Now - timePressedDown).TotalSeconds < tapTimeThreshold && !hasSlammed && !hasSwipedHorizontally)
+                {
+                    if (posisitionPressStart.x < Screen.width * 0.5f)
+                    {
+                        onInputRotate(true);   
+                    }
+                    else
+                    {
+                        onInputRotate(false);
+                    }
+                }
+            }
+            else if (Input.touches[0].phase == TouchPhase.Canceled)
+            {
+            }
+        }
+#endif
     }
 
 
